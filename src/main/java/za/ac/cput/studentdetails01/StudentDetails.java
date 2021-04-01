cd3e0cf5fbcfb388d1b7743d1097cce6666255fc
Breyton Ernstzen <breytonseanerstzen1224@gmail.com>
2021-04-01 02:15:58 +0200

    This is my first commit

---
+++ src/main/java/za/ac/cput/studentdetails01/StudentDetails.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.studentdetails01;
+
+/**
+ *
+ * @author Breyton Ernstzen - 217203027 (Group Lead)
+ */
+public class StudentDetails {
+    
+}
